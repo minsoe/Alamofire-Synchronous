e1113f7ed0be71fd1b4c0f4d819b2f9d023cc752
Min Soe Zan <minsoe@Min-MacBook-Pro.local>
2017-07-22 23:44:04 +0800

    package swift file
    added Package.swift file for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// Package.swift
+// Created by Min Soe Zan on 17/07/22.
+
+import PackageDescription
+
+let package = Package(
+    name: "Alamofire_Synchronouse",
+    dependencies : [
+        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
+    ]
+)
